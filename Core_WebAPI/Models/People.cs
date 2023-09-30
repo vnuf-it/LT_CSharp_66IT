acf13bc444dc060b3df0cde12b53c36ef2128b76
Hoa Tran Xuan <59261888+hoatx611@users.noreply.github.com>
2023-09-30 08:45:01 +0700

    Thêm Class Student là lớp con kế thừa từ Class People

--- Core_WebAPI/Models/People.cs
+++ Core_WebAPI/Models/People.cs
@@ -41,6 +41,5 @@
 
         public abstract string GetHoVaTen();
 
-
     }
 }
---
+++ Core_WebAPI/Models/Student.cs
@@ -0,0 +1,30 @@
+﻿namespace Core_WebAPI.Models
+{
+    public class Student : People
+    {
+        string msv; // 221070001 (22 năm | 107 mã ngành | 0001 số thứ tự nhập học)
+        string lop; // 66A-HTTT, 66B-HTTT
+        string khoavien;
+
+        public string Msv { get => msv; set => msv = value; }
+        public string Lop { get => lop; set => lop = value; }
+        public string Khoavien { get => khoavien; set => khoavien = value; }
+
+        public Student(string _hodem, string _ten) : base(_hodem, _ten)
+        {
+        }
+
+        public Student(string _hodem, string _ten, int _tuoi) : base(_hodem, _ten, _tuoi)
+        {
+        }
+
+        public Student(string _hodem, string _ten, string _dienthoai, string _email) : base(_hodem, _ten, _dienthoai, _email)
+        {
+        }     
+
+        public override string GetHoVaTen()
+        {
+            return $"{Hodem} {Ten}";
+        }
+    }
+}
