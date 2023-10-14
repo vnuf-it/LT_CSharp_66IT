132499cbab01dc9688ce32147c8424ac2689af46
Hoa Tran Xuan <59261888+hoatx611@users.noreply.github.com>
2023-10-14 15:26:46 +0700

    DemoController

---
+++ Core_WebApp_MVC/Controllers/DemoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Core_WebApp_MVC.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
