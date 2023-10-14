d6a8e682720820d396d3b4544c03bac3167cd7b2
Hoa Tran Xuan <59261888+hoatx611@users.noreply.github.com>
2023-10-14 16:34:37 +0700

    Call API and show Table SinhVien

---
+++ Core_WebApp_MVC/wwwroot/httt/js/myjs.js
@@ -0,0 +1,62 @@
+﻿const url = "http://localhost:63223/api/Student";
+
+// Cach 1:
+async function getapi(url) {
+    const tmp = await fetch(url,);
+    var data = await tmp.json();
+    console.log(data);
+    show_table(data);
+}
+getapi(url);
+
+
+// Cach 2:
+//async function getapi(url) {
+//    try {
+//        const response = await fetch(url);
+
+//        if (!response.ok) {
+//            throw new Error(`Error! status: ${response.status}`);
+//            console.log(response.status);
+//        }
+
+//        const result = await response.json();
+//        return result;
+//    } catch (err) {
+//        console.log(err);
+//    }
+//}
+
+//getapi();
+
+function show_table(arrs) {
+    let tb_row;
+    //tb_row = `<tr>
+    //    <td>(1)</td>
+    //    <td>(2)</td>
+    //    <td>(3)</td>
+    //    <td>(4)</td>
+    //    <td>(5)</td>
+    //    <td>(6)</td>
+    //    </tr>`;
+
+    arrs.forEach(function show(p) {
+        tb_row += `<tr>
+        <td>${p.msv} </td>
+        <td>${p.lop}</td>
+        <td>${p.khoavien}</td>
+        <td>${p.cccd}</td>
+        <td>${p.hodem} ${p.ten}</td>
+        <td>${p.tuoi}</td>
+        </tr>`;
+    });
+    
+    // Setting innerHTML as tab variable
+    document.getElementById("dsSinhVien").innerHTML = tb_row;
+}
+
+function clear_table() {
+    var clear = document.getElementById("dsSinhVien");
+    clear.remove();
+}
+
