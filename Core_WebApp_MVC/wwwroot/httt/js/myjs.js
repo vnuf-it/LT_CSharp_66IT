37aad72f2d6b181f48e87d4a42490e44d7d0def8
Hoa Tran Xuan <59261888+hoatx611@users.noreply.github.com>
2023-11-04 13:50:45 +0700

    Add SinhVien

--- Core_WebApp_MVC/wwwroot/httt/js/myjs.js
+++ Core_WebApp_MVC/wwwroot/httt/js/myjs.js
@@ -1,15 +1,44 @@
-﻿const url = "http://localhost:63223/api/Student";
+﻿const url = "http://localhost:5077/api/Student";
 let test;
 
 // Cach 1:
-async function getapi(url) {
-    const tmp = await fetch(url,);
-    var data = await tmp.json();
+async function getdata(url) {
+    const options = {
+        method: 'GET',
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "PUT,GET,POST,DELETE,OPTIONS",
+            "Access-Control-Allow-Headers": "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, Origin, X-Auth-Token"
+        }
+    }
+    const response = await fetch(url, options);
+    var data = await response.json();
     test = data;
     console.log(data);
     show_table(data);
 }
-getapi(url);
+getdata(url);
+
+
+async function adddata(data) {
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json; charset=utf-8",
+            "Access-Control-Allow-Origin": '*',
+            "Access-Control-Allow-Methods": 'PUT,GET,HEAD,POST,DELETE,OPTIONS'
+        },
+        body: JSON.stringify(data)
+    }
+    const response = await fetch(url, options);
+    var data = await response.json();
+    test = data;
+    console.log(data);
+    $('#addSinhVien').modal('hide');
+    clear_all();
+    show_table(data);
+}
 
 
 // Cach 2:
@@ -32,18 +61,17 @@ getapi(url);
 //getapi();
 
 function show_table(arrs) {
-    let tb_row;
-    //tb_row = `<tr>
-    //    <td>(1)</td>
-    //    <td>(2)</td>
-    //    <td>(3)</td>
-    //    <td>(4)</td>
-    //    <td>(5)</td>
-    //    <td>(6)</td>
-    //    </tr>`;
+    let tb_row = `<tr>
+        <td>(1)</td>
+        <td>(2)</td>
+        <td>(3)</td>
+        <td>(4)</td>
+        <td>(5)</td>
+        <td>(6)</td>
+        </tr>`;
 
     arrs.forEach(function show(p) {
-        tb_row += `<tr class="httt_row");>
+        tb_row += `<tr class="httt_row">
         <td>${p.msv} </td>
         <td>${p.lop}</td>
         <td>${p.khoavien}</td>
@@ -52,7 +80,7 @@ function show_table(arrs) {
         <td>${p.tuoi}</td>
         </tr>`;
     });
-    
+
     // Setting innerHTML as tab variable
     document.getElementById("dsSinhVien").innerHTML = tb_row;
 }
@@ -61,7 +89,39 @@ function clear_all() {
     document.querySelectorAll(".httt_row").forEach(el => el.remove());
 }
 
-function show_all() {
+function refresh_all() {
     show_table(test);
 }
 
+function save_sinhvien() {
+    //var msv = document.getElementById("msv").value;
+    //var lop = document.getElementById("lop").value;
+    //var khoavien = document.getElementById("khoavien").value;
+    //var cccd = document.getElementById("cccd").value;
+    //var hodem = document.getElementById("hodem").value;
+    //var ten = document.getElementById("ten").value;
+    //var tuoi = document.getElementById("tuoi").value;
+
+    var my_msv = $("#msv").val();
+    var my_lop = $("#lop").val();
+    var my_khoavien = $("#khoavien").val();
+    var my_cccd = $("#cccd").val();
+    var my_hodem = $("#hodem").val();
+    var my_ten = $("#ten").val();
+    var my_tuoi = $("#tuoi").val();
+
+    var data = {
+        "msv": my_msv,
+        "lop": my_lop,
+        "khoavien": my_khoavien,
+        "cccd": my_cccd,
+        "hodem": my_hodem,
+        "ten": my_ten,
+        "bietdanh": "",
+        "email": "",
+        "dienthoai": "",
+        "tuoi": my_tuoi
+    }
+
+    adddata(data);
+}
